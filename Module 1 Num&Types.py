c1df4458ab7022a57677c080f3cc0e2f48edef9f
Lauren358 <ldavis482@ivytech.edu>
2025-11-10 19:48:28 -0400

    Add files via upload

---
+++ Module 1 Num&Types.py
@@ -0,0 +1,7 @@
+seconds_per_hour=60*60
+print("Seconds per hour:", seconds_per_hour)
+seconds_per_day= seconds_per_hour*24
+print("Seconds per day:",seconds_per_day)
+print (seconds_per_day/seconds_per_hour)
+print (seconds_per_day//seconds_per_hour)
+#The number does match with the floating point divison resulting in the hours in a day
